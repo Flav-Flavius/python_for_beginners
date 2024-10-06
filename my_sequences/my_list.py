46772b1fc2525535126bb6bfde88333b98b939d3
oltea <olteaninardeal@gmail.com>
2024-10-06 11:49:18 +0300

    Initial draft

---
+++ my_sequences/my_list.py
@@ -0,0 +1,16 @@
+myList = ["one", 2, 'three']              # list with 3 items, has length 3
+myList1 = []                              # list with 0 items, has length 0
+print(myList)
+print(myList1)
+print("============================")
+
+# They look similar, but they are different types
+s = "100"
+myNewList = [100]
+i = 100
+print(s)
+print(myNewList)
+print(i)
+print(type(s))
+print(type(myNewList))
+print(type(i))
