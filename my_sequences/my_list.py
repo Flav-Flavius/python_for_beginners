442ec5466ba14629d74804ba6f968dffda0f4a76
oltea <olteaninardeal@gmail.com>
2024-10-07 10:00:04 +0300

    Initial draft

--- my_sequences/my_list.py
+++ my_sequences/my_list.py
@@ -11,6 +11,25 @@ i = 100
 print(s)
 print(myNewList)
 print(i)
-print(type(s))
-print(type(myNewList))
-print(type(i))
+print(type(s))   # Output: <class 'str'>
+print(type(myNewList))   # Output: <class 'list'>
+print(type(i))    # Output: <class 'int'>
+print("===========================")
+
+# Accessing elements
+my_list = [10, 20, 30, 40, 50]
+
+first_element = my_list[0]  # Accessing the first element (index 0)
+last_element = my_list[-1]  # Accessing the last element using negative indexing
+
+print(first_element) # Output: 10
+print(last_element)  # Output: 50
+print("==============================")
+
+# Modifying elements
+my_list = [10, 20, 30, 40, 50]
+my_list[2] = 35 # Modifying the element at index 2 (third element)
+
+print(my_list)   # Output: [10, 20, 35, 40, 50]
+print("=============================")
+
